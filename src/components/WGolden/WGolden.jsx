4803fc529221cc3c804c56c1b79f47f43474350f
fellipefreiire <fellipefreiire3@gmail.com>
2020-07-25 12:53:18 -0300

    new watchstones components

---
+++ src/components/WGolden/WGolden.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const WGolden = props => {
+	return (
+		<Container>
+			<div></div>
+		</Container>
+	);
+};
+
+export default WGolden;
--- src/components/WHold/WHold.jsx
+++ src/components/WHold/WHold.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 
 import Watchstone from "../Watchstone/Watchstone";
+import WCobalt from "../WCobalt/WCobalt";
+import WCrimson from "../WCrimson/WCrimson";
+import WGolden from "../WGolden/WGolden";
+import WViridian from "../WViridian/WViridian";
 import { Container } from "./styles";
 
 const WHold = props => {
 	return (
 		<Container>
 			<div className={`${props.watchstone_class}`}>
-				<Watchstone id={props.id} watchstone_src={props.watchstone} alt={props.watchstone_name} />
+				<WCrimson />
+				{/* <Watchstone id={props.id} watchstone_src={props.watchstone} alt={props.watchstone_name} /> */}
 			</div>
 		</Container>
 	);
