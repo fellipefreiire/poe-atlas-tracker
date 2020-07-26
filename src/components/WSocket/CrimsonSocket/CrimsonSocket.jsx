8a53c22b41b3f72dfa1b50111c2da1e3b6c02b73
fellipefreiire <fellipefreiire3@gmail.com>
2020-07-26 13:39:34 -0300

    crimson and viridian socket components

---
+++ src/components/WSocket/CrimsonSocket/CrimsonSocket.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Container from "./styles";
+
+const CrimsonSocket = props => {
+	return (
+		<Container>
+			<div></div>
+		</Container>
+	);
+};
+
+export default CrimsonSocket;
